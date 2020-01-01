60c96b28b25c3e5b15cb1cd0f9a020de8eb0eca5
Jonas Thiesen <jonas.k.thiesen@gmail.com>
2020-01-01 07:12:18 +0100

    Add implementation for writeFile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { isTypeOfString } from './utils.js'
+import { isTypeOfString } from './utils'
 import { isNil } from 'ramda'
 
 export const readFile = (filePath, options) => {
@@ -9,6 +9,24 @@ export const readFile = (filePath, options) => {
 
   const promise = new Promise((resolve, reject) => {
     fs.readFile(filePath, options, (err, data) => {
+      if (!isNil(err)) {
+        return reject(err)
+      }
+
+      return resolve(data)
+    })
+  })
+
+  return promise
+}
+
+export const writeFile = (filePath, data, options) => {
+  if (!isTypeOfString(filePath)) {
+    throw new TypeError('`filePath` must be of type `string`')
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    fs.writeFile(filePath, data, options, (err, data) => {
       if (!isNil(err)) {
         return reject(err)
       }
